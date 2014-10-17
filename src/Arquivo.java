c3d6f343db2ed41fbb60709e5a9c2549996a2379
André Minoro Fusioka <minorofusioka@hotmail.com>
2014-10-17 13:28:36 -0300

    Interface para sistema de arquivos

---
+++ src/Arquivo.java
@@ -0,0 +1,45 @@
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Classe que implementa operações básicas sobre um arquivo
+ * 
+ * @author minoro
+ *
+ */
+public class Arquivo {
+	
+	private String nome;
+	private Integer identificador;
+	private int posicao;
+	private float tamanho;
+	private String proprietario;
+	private Date dataCriacao;
+	private Date dataUltimaModificacao;
+	private String conteudo;
+	
+	/**
+	 * Instancia um arquivo vazio
+	 * 
+	 * @param nome - nome do arquivo a ser criado
+	 * @param proprietario - proprietario do arquivo
+	 */
+	Arquivo(String nome, String proprietario){
+		this.nome = nome;
+		this.proprietario = proprietario;
+		
+		this.posicao = 0;
+		this.tamanho = 0;
+		this.dataCriacao = Calendar.getInstance().getTime();
+		this.dataUltimaModificacao = Calendar.getInstance().getTime();
+		
+	}
+	
+	void alterarArquivo(){
+		this.tamanho = this.conteudo.length();
+		this.dataUltimaModificacao = Calendar.getInstance().getTime();
+	}
+	
+	
+	
+}
---
+++ src/SistemaArquivo.java
@@ -0,0 +1,84 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class SistemaArquivo extends UnicastRemoteObject implements SistemaArquivoInterface {
+
+	protected SistemaArquivo() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean criarArquivo(String caminho) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;	
+	}
+
+	@Override
+	public boolean deletarArquivo(String caminho) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean renomearArquivo(String caminhoOrigem, String caminhoDestino)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean moverArquivo(String caminhoOrigem, String caminhoDestino)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean copiarArquivo(String caminhoOrigem, String caminhoDestino)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String lerArquivo(String caminho) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean escreverArquivo(String caminho, String texto)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean escreverArquivo(String caminho, String texto, int posicao)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Arquivo getAtributes(String caminho) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAtributes(String caminho, Arquivo arquivo)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean autenticarUsuario(String nome) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
